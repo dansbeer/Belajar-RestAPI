caca944e8ffb7f2ccad142705f5b8d8c92b2b600
Pandya Arya Daniswara <mrsterbern@gmail.com>
2023-01-30 13:52:18 +0700

    title

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(title="My First Rest API")
 
-@app.get("/items/{item_id}")
+
+@app.get("/items/{item_id}", tags=["items"], summary="Read Item Only", description="Read Item Cooler")
 async def read_item(item_id: int):
-    return {"item_id":item_id}
+    return {"item_id": item_id}
